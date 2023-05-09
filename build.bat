92241b5a82f0b4fa98bd49198c6be2fbef2918c9
William Miles <wmpmiles@gmail.com>
2023-05-09 20:39:37 +1000

    Switched to a split debug/release build.

--- build.bat
+++ build.bat
@@ -1,1 +1,2 @@
-cl /std:c17 /arch:AVX2 /Wall /Zi e02.c /link /DEBUG:FULL
+cl /std:c17 /arch:AVX2 /Wall /Zi /Fe:debug.exe e02.c /link /DEBUG:FULL
+cl /std:c17 /arch:AVX2 /Wall /Zi /O2 /Fe:release.exe e02.c /link /DEBUG:FULL
